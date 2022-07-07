f898803dd6bb551949288839f552a101875a8d0a
Shidan Wang <sdw95927@gmail.com>
2022-07-06 20:03:48 -0500

    Update plot_colorbar.py

--- plot_colorbar.py
+++ plot_colorbar.py
@@ -8,3 +8,11 @@ plt.xticks([], [])
 plt.yticks(np.linspace(colorbar.shape[0], 0, 10), np.round(np.linspace(vmin, vmax, 10), 1))
 plt.ylim(150, 0)
 plt.show()
+
+
+# Change xticks of colorbar
+cbar = plt.colorbar()
+cbar.ax.get_yaxis().set_ticks([])
+ys = [(N_CLASSES-1)/N_CLASSES * (i + 0.5) for i in range(N_CLASSES)]
+for i, category in enumerate(list(CLASSES_NAMES)):
+    cbar.ax.text(10, ys[i], category, ha='left', va='center')
