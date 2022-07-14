6932d1ce65cf2a3043c657ad535f19a65e6fa67f
Shidan Wang <sdw95927@gmail.com>
2022-07-14 16:21:41 -0500

    Update plot_colorbar.py

--- plot_colorbar.py
+++ plot_colorbar.py
@@ -1,4 +1,4 @@
-# Plot colorbar
+### Plot a single colorbar
 vmax = 2.5
 vmin = -0.5
 colorbar = list(np.linspace(vmax, vmin, 150))*10
@@ -9,10 +9,24 @@ plt.yticks(np.linspace(colorbar.shape[0], 0, 10), np.round(np.linspace(vmin, vma
 plt.ylim(150, 0)
 plt.show()
 
+### Plot colorbar aside of a main figure
+from matplotlib.colorbar import ColorbarBase
 
-# Change xticks of colorbar
+fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+plt.imshow(image)
+ax2 = fig.add_axes([0.95, 0.1, 0.03, 0.8])
+bounds = np.linspace(0, vmax, 20)
+cb = ColorbarBase(ax2, cmap=cmap, norm=norm,
+    spacing='proportional', ticks=bounds, boundaries=bounds, format='%.2f')
+
+ax.set_title("Slide {}, Region {}".format(slide_id, patch_id))
+ax2.set_ylabel('SCC Score', size=12)
+
+
+### Change xticks of colorbar
 cbar = plt.colorbar()
 cbar.ax.get_yaxis().set_ticks([])
 ys = [(N_CLASSES-1)/N_CLASSES * (i + 0.5) for i in range(N_CLASSES)]
 for i, category in enumerate(list(CLASSES_NAMES)):
     cbar.ax.text(10, ys[i], category, ha='left', va='center')
+
