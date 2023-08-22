e0d9aaee869e6e44114a54d885f6ffc3276ca8a8
Shidan Wang <sdw95927@gmail.com>
2023-08-22 10:21:26 +0800

    Update 3d_plot.py

--- 3d_plot.py
+++ 3d_plot.py
@@ -36,6 +36,15 @@ ax = fig.add_subplot(111, projection='3d')
 polyc = Poly3DCollection(verts[faces], facecolors=[cm.jet(_/vmax) for _ in colors], alpha=0.5, rasterized=True)
 # polyc.set_array(vs)
 # polyc.set_clim(1, 900)
+# Appearance settings
+ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+ax.xaxis.pane.fill = False
+ax.yaxis.pane.fill = False
+ax.zaxis.pane.fill = False
+ax.grid(False) 
+
 ax.add_collection(polyc)
 ax.set_xlim(0, 50)
 ax.set_ylim(0, 50)
